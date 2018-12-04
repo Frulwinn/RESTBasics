52b35741c431abc3ed90ed7fa2d20d5a2e194696
Frulwinn <frulwinn@gmail.com>
2018-12-04 14:17:55 -0800

    added func searchForPeople in PersonController

--- RESTBasics/RESTBasics/PersonController.swift
+++ RESTBasics/RESTBasics/PersonController.swift
@@ -3,5 +3,15 @@ import Foundation
 private let  baseURL = URL(string: "https://swapi.co/api/people/")!
 
 class PersonController {
-    
+    func searchForPeople(with searchTerm: String, completion: @escaping ([Person]?, Error?) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)!
+        let searchQuaryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents.queryItems = [searchQuaryItem]
+        
+        guard let requestURL = urlComponents.url else {
+            NSLog("problem constructing search URL for \(searchTerm)")
+            completion(nil, NSError())
+            return
+        }
+    }
 }
